2cc585a3b2bda61beff4e5cad2b5bbf1a4e2939c
Oleh Kriuchenkov <Oleh.Kriuchenkov@nike.com>
2022-07-26 13:53:27 +0300

    create more collections section

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HelloContent } from "./Sections/HelloContent";
 import { TopSelling } from "./Sections/TopSelling";
 import { CreateACustomRing } from "./Sections/CreateACustomRing";
+import { MoreCollections } from "./Sections/MoreCollections";
 
 import './MainPage.css';
 
@@ -12,6 +13,7 @@ export const MainPage = ({ isMainShadowed } : { isMainShadowed: boolean }) => {
             <HelloContent isMainShadowed />
             <TopSelling />
             <CreateACustomRing />
+            <MoreCollections />
         </main>
     );
 };
---
+++ src/pages/MainPage/Sections/MoreCollections.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Button } from "../../../components/Button/Button";
+
+import moreCollectionItem1 from '../../../assets/images/MoreCollectionItem1.png';
+import moreCollectionItem2 from '../../../assets/images/MoreCollectionItem2.png';
+import moreCollectionItem3 from '../../../assets/images/MoreCollectionItem3.png';
+import moreCollectionItem4 from '../../../assets/images/MoreCollectionItem4.png';
+import moreCollectionItem5 from '../../../assets/images/MoreCollectionItem5.png';
+import moreCollectionItem6 from '../../../assets/images/MoreCollectionItem6.png';
+
+import './MoreCollections.css';
+
+const moreCollectionsItems = [
+    {
+        text: 'The paradise collection',
+        img: moreCollectionItem1,
+    },
+    {
+        text: 'Hoops & studs',
+        img: moreCollectionItem2,
+    },
+    {
+        text: 'Rings & Stackables',
+        img: moreCollectionItem3,
+    },
+    {
+        text: 'The paradise collection',
+        img: moreCollectionItem4,
+    },
+    {
+        text: 'Hoops & studs',
+        img: moreCollectionItem5,
+    },
+    {
+        text: 'Rings & Stackables',
+        img: moreCollectionItem6,
+    },
+];
+
+export const MoreCollections = () => {
+
+    return (
+        <section className="more-collections-container">
+            <p className="more-collections-title">More collections</p>
+            <div className="more-collections-items">
+                {moreCollectionsItems.map((item) => {
+                    return (
+                        <div key={item.img} className="more-collections-item">
+                            <div className="more-collection-item-picture">
+                                <img src={item.img} width={447} height={244} alt="more collections item" />
+                            </div>
+                            <p>{item.text}</p>
+                        </div>
+                    );
+                })}
+            </div>
+            <Button
+                text="SHOP NOW"
+                type="bordered"
+                isRounded
+                width={234}
+                onClick={() => alert('Shop')}
+            />
+        </section>
+    );
+};
